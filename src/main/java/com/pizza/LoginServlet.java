5da676c08e5e2c51aed04816fb607bd54ff87bab
Andrew Valancius <avalancius6@gmail.com>
2019-08-08 10:45:43 -0400

    Add DB connection

--- src/main/java/com/pizza/LoginServlet.java
+++ src/main/java/com/pizza/LoginServlet.java
@@ -27,20 +27,41 @@ public class LoginServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		if (request.getParameter("isError") != null) {
+			out.println("Please login first");
+		}
 		out.println("<h2>Login to your account</h2>\r\n" + 
-				"        <form id=\"login\" action=\"dashboard\" method=\"POST\">\r\n" + 
+				"        <form id=\"login\" action=\"home\" method=\"POST\">\r\n" + 
 				"          <input type=\"text\" name=\"username\" placeholder=\"username\" />\r\n" + 
 				"          <input type=\"password\" name=\"password\" placeholder=\"password\" />\r\n" + 
 				"          <button type=\"submit\" value=\"login\" class=\"btn\">Login</button>\r\n" + 
 				"        </form>");
+		
+		
+		
+		out.println("</html>");
+		
+		
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+
+		//TODO check user name and password
+		// OrderDao.checkUser(request.getParameter(username, password)
+		if (1 == 2) {
+			// redirect to order page
+		} else {
+			// print out error message
+			// forward back to self
+			PrintWriter out = response.getWriter();
+			request.setAttribute("isError", true);
+			doGet(request, response);
+		}
+		
 	}
 
 }
